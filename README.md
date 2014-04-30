52db607ebb96298decea82557f52bc38f43d2034
tedshd <tedshd@gmail.com>
2014-05-01 00:59:14 +0800

    [Dev] Add addClass & removeClass.

--- README.md
+++ README.md
@@ -6,22 +6,41 @@ Some method use easy in JavaScript
 ## node
 
 Like jQuery node selector
+
 $('div') = node('div')
+
 Use in IE8 up
 
 ## indexInParent
 
 Like jQuery index()
+
 indexInParent(<DOM Element>)
+
 It can find current element index from parent DOM element
 
 ## delElement
 
 Delete DOM element
+
 delElement(<DOM Element>)
+
+## addClass
+
+Add class to element
+
+addClass(<DOM Element>, <class name>)
+
+## removeClass
+
+remove class to element
+
+removeClass(<DOM Element>, <class name>)
 
 ## toHHMMSS
 
 Number trans to time
+
 toHHMMSS(<Number>)
+
 Time format is HH:MM:SS
--- javascript/lightweight_js_lib.js
+++ javascript/lightweight_js_lib.js
@@ -41,6 +41,25 @@ function delElement(dom) {
     }
 }
 
+// add class
+function addClass(dom, className) {
+    if (dom.classList) {
+        dom.classList.add(className);
+    } else {
+        dom.className += ' ' + className;
+    }
+}
+
+// remove class
+function removeClass(dom, className) {
+    if (dom.classList) {
+        dom.classList.remove(className);
+    } else {
+        var reg = new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi');
+        dom.className = dom.className.replace(reg, ' ');
+    }
+}
+
 // handle time
 function toHHMMSS(sec_num) {
     var hours   = Math.floor(sec_num / 3600),
