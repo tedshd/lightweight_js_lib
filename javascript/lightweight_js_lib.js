9ced3129382e38ac5f0542a82d0957b58ca122e8
tedshd <tedshd@gmail.com>
2017-01-02 20:45:25 +0800

    [dev] Add timestramp to date.

--- javascript/lightweight_js_lib.js
+++ javascript/lightweight_js_lib.js
@@ -81,6 +81,19 @@ function toHHMMSS(sec_num) {
     return time;
 }
 
+function timeConverter(timestamp){
+  var a = new Date(timestamp * 1000);
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+
 // handel cookie
 function setCookie(option)
 {
